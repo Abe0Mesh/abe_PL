993c3ae177ec43708752f3581147785790094200
Abe0Mesh <abe.meshesha@gmail.com>
2026-04-05 00:59:50 -0700

    built basic lexer for 3 tokens, and assembly translation

---
+++ CMakeFiles/abe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abe.dir/src/main.cpp.o"
+  "CMakeFiles/abe.dir/src/main.cpp.o.d"
+  "abe"
+  "abe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
